74552704b304affd10a08fbd5202dccbe64758cc
anand1221178 <anandpatel1221178@gmail.com>
2024-04-24 19:54:53 +0200

    comments explaining code

--- PC_assignment1_2024/base_codes/src/fractal.cpp
+++ PC_assignment1_2024/base_codes/src/fractal.cpp
@@ -13,10 +13,11 @@
 #include <omp.h>
 using namespace std;
 
-#define DIM 768
+#define DIM 768 //defines the image dimensions width and height
 /*Uncomment the following line for visualization of the bitmap*/
 #define DISPLAY 1
 
+//struct used in the julia function to represent complex numbers on the complex plane
 struct cuComplex {
     float   r;
     float   i;
@@ -30,23 +31,27 @@ struct cuComplex {
     }
 };
 
+//calculates the membership of a point in the complex plane within the Julia set
+//x is the x-coordinate of the pixel in image, y is the y-coordinate of the pixel in image
 int julia( int x, int y ) { 
     const float scale = 1.5;
+    //calculates sacled versions of x and y -> transforms the pixel coordinates into comlpex plane coordinates suitable for the julia set formula
     float jx = scale * (float)(DIM/2 - x)/(DIM/2);
     float jy = scale * (float)(DIM/2 - y)/(DIM/2);
 
     //cuComplex c(-0.8, 0.156);
-    cuComplex c(-0.7269, 0.1889);
-    cuComplex a(jx, jy);
+    cuComplex c(-0.7269, 0.1889); //defines object c -> changing this will give us a different julia set
+    cuComplex a(jx, jy);// defines object a -> created using the scaled coordinates (jx, jy) asscoiated with the pixel (x, y)
 
+    //iterates a max of 200 times to determine if the point is in the julia set
     int i = 0;
     for (i=0; i<200; i++) {
-        a = a * a + c;
-        if (a.magnitude2() > 1000)
-            return 0;
+        a = a * a + c; //a is squared and added with constant c 
+        if (a.magnitude2() > 1000) //squared magnitude of a is compared with 1000 for our divergence check
+            return 0; //if the magnitude of a is greater than 1000, the point is not in the julia set
     }
 
-    return 1;
+    return 1; //if the point is in the julia set
 }
 
 /*Parallelize the following function using OpenMP*/
@@ -64,16 +69,18 @@ void kernel_omp ( unsigned char *ptr ){
     }
  }
  
- void kernel_serial ( unsigned char *ptr ){
-    for (int y=0; y<DIM; y++) {
-        for (int x=0; x<DIM; x++) {
-            int offset = x + y * DIM;
 
-            int juliaValue = julia( x, y );
-            ptr[offset*4 + 0] = 255 * juliaValue;
-            ptr[offset*4 + 1] = 0;
-            ptr[offset*4 + 2] = 0;
-            ptr[offset*4 + 3] = 255;
+ //responsible for calculating and assigning colors to pixels in the image
+ void kernel_serial ( unsigned char *ptr ){ //send in a pointer to an array of unsigned chars -> prolly reps image data in memory RGB?
+    for (int y=0; y<DIM; y++) { //iterate over the rows of the image
+        for (int x=0; x<DIM; x++) { //iterate over the columns of the image
+            int offset = x + y * DIM; //used to locate memory location of the pixel in the image data array pointed to by ptr
+
+            int juliaValue = julia( x, y ); //determines if the pixel at (x, y) is in the julia set
+            ptr[offset*4 + 0] = 255 * juliaValue; //assigns the color of the pixel based on the juliaValue
+            ptr[offset*4 + 1] = 0; //green
+            ptr[offset*4 + 2] = 0; //blue
+            ptr[offset*4 + 3] = 255; //alpha
         }
     }
  }
